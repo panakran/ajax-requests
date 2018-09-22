c8b537bc0f4ca734f0ea583b7566b3ead7da5de8
unknown <panos_kr@hotmail.com>
2018-09-22 16:38:06 +0300

    adding scroll bar to textareas, fixing code, adding comments

---
+++ js/main.js
@@ -0,0 +1,167 @@
+/**
+ * Initial global constants
+ */
+const selectElem = document.querySelectorAll('select');
+const baseURL = document.getElementById("baseurl");
+const url = document.getElementById("url");
+const autocompleteinstanceBaseUrl = M.Autocomplete.init(baseURL);
+const autocompleteinstanceUrl = M.Autocomplete.init(url);
+
+
+/**
+ * Initialize method sets up the select dropdown
+ */
+const initMethod = () => M.FormSelect.init(selectElem);
+
+/**
+ * Executes a funciton passed with its params if exeption throwed return the first param passed
+ * @param {type} func
+ * @param {type} params
+ * @return {unresolved}
+ */
+const catchJsonException = (func, ...params) => {
+    try {
+        return func(...params);
+    } catch (e) {
+        return params;
+    }
+};
+
+/**
+ * Executes native JSON.stringify but with error catching
+ * @param {type} value
+ */
+const stringifyJSON = (value) => catchJsonException(JSON.stringify, value, undefined, 4);
+
+/**
+ * Executes native JSON.parse but with error catching
+ * @param {type} value
+ */
+const parseJson = (value) => catchJsonException(JSON.parse, value);
+
+/**
+ * main render method we pass the object with new elements and values to render
+ * we also pass success true/false to handle rendering for success/error request
+ * @param {type} renderObj
+ * @return {undefined}
+ */
+const renderDOM = (renderObj) => {
+    renderNewInnerHTML(renderObj.timerElement, createTimerText(renderObj.finalRequestTime));
+    addToAutocompleteInstance(autocompleteinstanceBaseUrl, renderObj.baseURL);
+    addToAutocompleteInstance(autocompleteinstanceUrl, renderObj.url);
+    if (renderObj.success) {
+        renderNewValue(renderObj.responseElementH, stringifyJSON(renderObj.response.headers));
+        renderNewValue(renderObj.responseElementB, stringifyJSON(renderObj.response.data));
+    } else {
+        printErrorMessage(renderObj.response);
+    }
+};
+
+/**
+ * adds a new object to autocomplete element
+ * @param {type} instance
+ * @param {type} newElement
+ */
+const addToAutocompleteInstance = (instance, newElement) =>
+    instance.updateData({...instance.options.data, [newElement]: null});
+/**
+ * prints error messages to DOM through M.toast method
+ * @param {type} msg
+ * @return {Function}
+ */
+const printErrorMessage = (msg) =>
+    M.toast({classes: "rounded card-panel red white-text text-darken-4 z-depth-5", html: `<p><code>${msg}</code></p>`});
+
+/**
+ * create text that will render to timer element
+ * @param {type} time
+ * @return {String}
+ */
+const createTimerText = (time) =>
+    (time).toFixed(2) + "ms";
+
+/**
+ * render new value to innerHTML
+ * @param {type} element
+ * @param {type} newValue
+ */
+const renderNewInnerHTML = (element, newValue) =>
+    element.innerHTML = newValue;
+
+/**
+ * render new value to value
+ * @param {type} element
+ * @param {type} newValue
+ */
+const renderNewValue = (element, newValue) =>
+    element.value = newValue;
+/**
+ * calculate exec time based on start and end time(simple sub)
+ * @param {type} endT
+ * @param {type} startT
+ * @return {Number}
+ */
+const calcExecTime = (endT, startT) => endT - startT;
+
+initMethod();
+
+/**
+ * on click submit button 
+ * 1. we prepare the axios config
+ * 2. on response we call renderDOM to update view
+ * @return {undefined}
+ */
+document.getElementById("req").onclick = () => {
+    const methodElement = document.getElementById("methods");
+    const method = methodElement.options[methodElement.selectedIndex].text;
+    const baseURL = document.getElementById("baseurl").value;
+    const url = document.getElementById("url").value;
+    const headersElement = document.getElementById("headers");
+    const bodyElement = document.getElementById("body");
+    const data = parseJson(bodyElement.value);
+    const headers = parseJson(headersElement.value);
+
+    let responseElementH = document.getElementById("responseH");
+    let responseElementB = document.getElementById("responseB");
+    let timerElement = document.getElementById("executiontime");
+
+
+    const axiosConfig = {method, url, baseURL, data, headers};
+    console.log("REQUEST::", axiosConfig);
+    const startTime = performance.now();
+    axios(axiosConfig)
+            .then((response) => {
+                const endTime = performance.now();
+                const finalRequestTime = calcExecTime(endTime, startTime);
+                console.log("RESPONSE::", response);
+                const renderObject = {
+                    responseElementH,
+                    responseElementB,
+                    timerElement,
+                    response,
+                    baseURL,
+                    url,
+                    finalRequestTime,
+                    success: true
+                };
+
+                renderDOM(renderObject);
+            })
+            .catch((response) => {
+                const endTime = performance.now();
+                const finalRequestTime = calcExecTime(endTime, startTime);
+                console.error("ERROR::", response);
+
+                const renderObject = {
+                    responseElementH,
+                    responseElementB,
+                    timerElement,
+                    response,
+                    baseURL,
+                    url,
+                    finalRequestTime,
+                    success: false
+                };
+                renderDOM(renderObject);
+            });
+};
